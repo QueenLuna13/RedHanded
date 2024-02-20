80daca7640cb873134a7bb54945b7f727b6d6cf7
ChelseaSoto <ch951840@ucf.edu>
2024-02-20 02:06:44 -0500

    Fixed Pause Menu and added Flashlight overlay
    Implemented Pause Menu into main scene, fixed some bugs.
    - Camera now stops moving during pause screen.
    - Game resets when going back to main menu after pausing game.
    
    Flashlight script uodated
    - When the game starts, player is told by UI overlay F key turns flashlight on and off
    
    PickupItem and obtained[item] overlays added but still need to be coded in.

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,27 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Flashlight : MonoBehaviour
 {
     public Light spotlight;
+    public GameObject overlayCanvas;
+    public GameObject overlayText;
+
+    private bool overlayShown = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(spotlight == null){
+        if (spotlight == null){
             spotlight = GetComponentInChildren<Light>();
+        }
+
+        if (overlayCanvas != null && overlayText != null){
+            overlayCanvas.SetActive(true);
+            overlayText.SetActive(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (spotlight != null)
-            {
-                spotlight.enabled = !spotlight.enabled;
+        if (Input.GetKeyDown(KeyCode.F)){
+            if (overlayShown){
+                if (spotlight != null){
+                    spotlight.enabled = !spotlight.enabled;
+                }
+
+                
+                overlayShown = false;
+                if (overlayCanvas != null){
+                    overlayCanvas.SetActive(false);
+                    overlayText.SetActive(false);
+                }
+            }else{
+                if (spotlight != null){
+                    spotlight.enabled = !spotlight.enabled;
+                }
             }
         }
     }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     
     public GameObject pauseMenuUI;
+    public GameObject playerCamera;
 
     // Update is called once per frame
     void Update()
@@ -24,11 +26,13 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false); //turns off canvas
         Time.timeScale = 1f; //continues in-game time
         GameIsPaused = false; //lets script know game is not paused
+        playerCamera.GetComponent<CameraController>().enabled = true; // Enable camera controller script
     }
 
     void Pause(){
         pauseMenuUI.SetActive(true); //turns on canvas
         Time.timeScale = 0f; //stops time
         GameIsPaused = true; //lets script know that game is paused
+        playerCamera.GetComponent<CameraController>().enabled = false; // Disable camera controller script
     }
 }
