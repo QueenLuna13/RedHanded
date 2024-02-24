c47792d6e82ebc5f5388e51feb4ab3b3736d7442
Alana Ackley <93610792+QueenLuna13@users.noreply.github.com>
2024-02-23 20:43:53 -0500

    push

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,57 +1,53 @@
 using UnityEngine;
+using TMPro;
 
 public class Pickup : MonoBehaviour
 {
-    public LayerMask itemLayer; // LayerMask to filter which objects can be picked up
-    public float interactDistance = 3f; // Distance at which the player can interact with objects
-    public GameObject pickupUI; // UI element to display when the player is near a pickable item
-    public GameObject obtainedKeyUI; // UI element to display when the player obtains the key
-    public GameObject obtainedStatueUI; // UI element to display when the player obtains the statue
+    public LayerMask itemLayer; 
+    public float interactDistance = 3f; 
+    public GameObject pickupUI; 
+    public GameObject obtainedKeyUI; 
+    public GameObject obtainedStatueUI; 
+    public TextMeshProUGUI keyCountText;
+    public TextMeshProUGUI statueText;
 
-    private GameObject itemToPickup; // Reference to the currently interactable item
+    private GameObject itemToPickup; 
+    private int keyCount = 0; 
+    private bool hasStatue = false; 
 
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, itemLayer))
-        {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, itemLayer)){
             itemToPickup = hit.collider.gameObject;
             pickupUI.SetActive(true);
 
-            if (Input.GetKeyDown(KeyCode.E))
-            {
+            if (Input.GetKeyDown(KeyCode.E)){
                 PickUpItem();
             }
-        }
-        else
-        {
+        }else{
             itemToPickup = null;
             pickupUI.SetActive(false);
         }
     }
 
-    void PickUpItem()
-    {
-        // Determine which obtained UI to show based on the type of item picked up
-        if (itemToPickup.CompareTag("Key"))
-        {
-            obtainedKeyUI.SetActive(true);
-            Invoke("HideObtainedUI", 2f); // Hide the obtained UI after 2 seconds
-        }
-        else if (itemToPickup.CompareTag("Statue"))
-        {
-            obtainedStatueUI.SetActive(true);
-            Invoke("HideObtainedUI", 2f); // Hide the obtained UI after 2 seconds
+    void PickUpItem(){
+        if (itemToPickup.CompareTag("Key")){
+            keyCount++; 
+            UpdateKeyCountUI();
+        }else if (itemToPickup.CompareTag("Statue")){
+            hasStatue = true;
+            UpdateStatueUI();
         }
 
-        // Destroy the item in the world
         Destroy(itemToPickup);
     }
 
-    void HideObtainedUI()
-    {
-        // Hide both obtained UIs
-        obtainedKeyUI.SetActive(false);
-        obtainedStatueUI.SetActive(false);
+    void UpdateKeyCountUI(){
+        keyCountText.text = keyCount.ToString();
+    }
+
+    void UpdateStatueUI(){
+        statueText.text = "1"; 
     }
 }
