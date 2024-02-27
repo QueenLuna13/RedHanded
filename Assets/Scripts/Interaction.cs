30ad66fb44a09e4de23dead2583715014eaa71b0
ChelseaSoto <ch951840@ucf.edu>
2024-02-27 00:42:12 -0500

    finished interaction script and door animation is now functioning.

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 
-public class Pickup : MonoBehaviour
+public class Interaction : MonoBehaviour
 {
     public LayerMask itemLayer;
     public float interactDistance = 3f;
@@ -10,7 +9,8 @@ public class Pickup : MonoBehaviour
     public GameObject obtainedKeyUI;
     public GameObject obtainedStatueUI;
     public GameObject doorOpenUI;
-
+    public GameObject doorStatueUI;
+    public GameObject doorExitUI;
     public TextMeshProUGUI keyCountText;
     public TextMeshProUGUI statueText;
 
@@ -18,59 +18,79 @@ public class Pickup : MonoBehaviour
     private int keyCount = 0;
     private int statueCount = 0;
 
+    private bool statueDoorOpened = false;
+
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, itemLayer)) {
+        bool isInteractable = Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, itemLayer);
+        if (isInteractable)
+        {
             interact = hit.collider.gameObject;
-            doorOrPickup();
-            if (Input.GetKeyDown(KeyCode.E)) {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
                 Pressed();
             }
-        } else {
+            else
+            {
+                doorOrPickup();
+            }
+        }
+        else
+        {
             interact = null;
             pickupUI.SetActive(false);
             doorOpenUI.SetActive(false);
         }
+
+        if (statueDoorOpened && interact != null && interact.CompareTag("statueDoor"))
+        {
+            doorOpenUI.SetActive(false);
+            doorStatueUI.SetActive(false);
+        }
     }
+
     void doorOrPickup()
     {
         if (interact.CompareTag("Key") || interact.CompareTag("Statue"))
         {
             pickupUI.SetActive(true);
+            doorOpenUI.SetActive(false);
         }
         else
         {
+            pickupUI.SetActive(false);
             doorOpenUI.SetActive(true);
         }
     }
+
     void Pressed()
     {
         if (interact.CompareTag("Key") || interact.CompareTag("Statue"))
         {
             PickUpItem();
         }
-        else
+        else if (interact.CompareTag("statueDoor") || interact.CompareTag("exitDoor"))
         {
             openDoor();
         }
     }
 
-
-
-    void PickUpItem() {
-        if (interact.CompareTag("Key")) {
+    void PickUpItem()
+    {
+        if (interact.CompareTag("Key"))
+        {
             obtainedKeyUI.SetActive(true);
             Invoke("HideObtainedUI", 2f);
             keyCount++;
             UpdateKeyCountUI();
         }
-        else if (interact.CompareTag("Statue")) {
+        else if (interact.CompareTag("Statue"))
+        {
             obtainedStatueUI.SetActive(true);
             Invoke("HideObtainedUI", 2f);
             statueCount++;
             UpdateStatueCountUI();
-
         }
 
         Destroy(interact);
@@ -78,40 +98,49 @@ public class Pickup : MonoBehaviour
 
     void openDoor()
     {
+        Animator doorAnimator = interact.transform.parent.GetComponent<Animator>();
         if (interact.CompareTag("statueDoor"))
         {
-            if (keyCount == 2)
+            if (keyCount >= 2)
             {
-                //play animation
-                keyCount = keyCount - 2;
+                doorAnimator.SetTrigger("OpenStatue");
+                keyCount -= 2;
                 UpdateKeyCountUI();
+                statueDoorOpened = true;
             }
             else
             {
-                //ui saying the door is locked
+                doorStatueUI.SetActive(true);
+                Invoke("HideObtainedUI", 2f);
             }
         }
         else if (interact.CompareTag("exitDoor"))
         {
-            if (statueCount == 1)
+            if (statueCount >= 1)
             {
-                //play animation
+                doorAnimator.SetTrigger("OpenExit");
                 statueCount--;
                 UpdateStatueCountUI();
             }
             else
             {
-                //ui saying you're here for the statue
+                doorExitUI.SetActive(true);
+                Invoke("HideObtainedUI", 2f);
             }
         }
     }
 
-    void HideObtainedUI() {
+
+    void HideObtainedUI()
+    {
         obtainedKeyUI.SetActive(false);
         obtainedStatueUI.SetActive(false);
+        doorExitUI.SetActive(false);
+        doorStatueUI.SetActive(false);
     }
 
-    void UpdateKeyCountUI() {
+    void UpdateKeyCountUI()
+    {
         keyCountText.text = keyCount.ToString();
     }
 
